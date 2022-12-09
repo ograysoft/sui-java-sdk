ee4720d542e762c7becc101a2773e4e6f4a19483
Ogray Software <ograysoft@gmail.com>
2022-12-09 13:10:43 +0100

    command line arguments processing fixed in class Wallet

--- src/main/java/org/ogray/sui/samples/Wallet.java
+++ src/main/java/org/ogray/sui/samples/Wallet.java
@@ -9,16 +9,25 @@ import java.security.SecureRandom;
 
 public class Wallet {
     public Wallet() {
+    }
 
+    static void showUsage() {
+        System.out.println("Usage: wallet <command> <parameter>");
+        System.out.println("   getobjects <address>");
+        System.out.println("   generate");
     }
     public static void main(String []args) {
-        if(args.length<2) {
-            System.out.println("Usage: wallet <command> <parameter>");
-            System.out.println("   getobjects <address>");
+        if(args.length<1) {
+            showUsage();
             return;
         }
+
         switch(args[0]) {
             case "getobjects":
+                if(args.length<2) {
+                    showUsage();
+                    return;
+                }
                 SUI sui = new SUI();
                 SuiJsonResponse resp = sui.sui_getObjectsOwnedByAddress(args[1]);
                 System.out.println(resp);
